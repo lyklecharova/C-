075e68f97a8bb16071149fc94d842c3c2703c80c
Лиляна Клечарова <lklecharova9@gmail.com>
2024-10-05 12:31:56 +0300

    First program in C++

---
+++ demo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// iostream - input/output stream
+// std - standard
+// cout - console output
+// endl - end of line
+
+int main()
+{
+    cout << "Hello World!" << endl;
+}
