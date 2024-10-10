868e56636dcfd7133cf1859dce6beb0cad71c786
Лиляна Клечарова <lklecharova9@gmail.com>
2024-10-10 22:03:07 +0300

    new program

---
+++ printProgram.cpp
@@ -0,0 +1,35 @@
+#include <cstdlib>
+#include <iostream>
+#include <windows.h> // Необходимо е за SetConsoleOutputCP и SetConsoleCP
+using namespace std;
+
+int main(int args, char *argv[])
+{
+    // Задаваме кодирането на конзолата на UTF-8
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    // cout << "Infomation" << endl;
+    // cout << "And" << endl;
+    // cout << "Computer" << endl;
+    // cout << "Engineering" << endl;
+    // cout << 2024 << "-" << 2024 + 1 << endl;
+
+    int a;
+    cout << "Въведете цяло число: " << endl;
+    // cin - console input
+    cin >> a;
+    cout << "a = " << a << endl;
+    cout << "a + 2 = " << a + 2 << endl;
+
+    int b;
+    cout << "Въведете цяло число: " << endl;
+    cin >> b;
+    cout << "a * b = " << a * b << endl;
+    cout << "b * 4 = " << b * 4 << endl;
+
+    cin.ignore(); // Игнорирай новия ред от предишния вход
+    cout << "Натиснете Enter, за да затворите терминала..." << endl;
+    cin.get(); // Изчаква натискане на Enter
+
+    return 0;
+}
