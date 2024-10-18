689721dd49f4b9bf5f9bfb166bdb33569d340948
Лиляна Клечарова <lklecharova9@gmail.com>
2024-10-18 12:33:30 +0300

    add

---
+++ Data abstraction/03_radius.cpp
@@ -0,0 +1,24 @@
+#include <cstdlib>
+#include <iostream>
+#include <windows.h> // Необходимо е за SetConsoleOutputCP и SetConsoleCP
+using namespace std;
+
+int main()
+{
+    // Настройка на конзолата на UTF-8 за коректно показване на текст
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    const double PI = 3.1415;
+
+    double r;
+    double p = 2 * PI * r;
+    double s = PI * r * r;
+
+    cin >> r;
+    cout << "p = " << p << "\n";
+    cout << "s = " << s << "\n";
+
+    system("pause");
+    return EXIT_SUCCESS;
+}
