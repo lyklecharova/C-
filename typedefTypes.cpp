db183ec34e694e28bd23802efebc779d79b731c4
Lilyana Klecharova <lklecharova9@gmail.com>
2024-10-27 22:28:05 +0200

    update

--- typedefTypes.cpp
+++ typedefTypes.cpp
@@ -2,8 +2,10 @@
 #include <vector>
 
 // typedef std::vector<std::pair<std::string, int>> pairlist_t;
-typedef std::string text_t;
-typedef int number_t;
+// typedef std::string text_t;
+// typedef int number_t;
+using text_t = std::string;
+using number_t = int;
 int main()
 {
     // typedef = reserved keyword used to create an additional name
