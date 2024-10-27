c11412fca47aa1ea8e6ef5319976fda05ddcd6cc
Lilyana Klecharova <lklecharova9@gmail.com>
2024-10-27 21:59:03 +0200

    add

---
+++ dataTypes.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+int main()
+{
+    // integer (whole number)
+    int age = 21;
+    int year = 2024;
+    int days = 7;
+
+    // double (number including decimal point)
+    double price = 10.99;
+    double gpa = 2.5;
+    double temperature = 25.1;
+
+    // single character
+    char grade = 'A';
+    char initial = 'B';
+    char currency = '$';
+
+    // boolen (true or false)
+    bool student = false;
+    bool power = true;
+    bool forSale = true;
+
+    // string (objects that represents a sequence of text)
+    std::string name = "John";
+    std::cout << "Hello, !" << name;
+
+    /*
+      // int x; // declaration
+    int x = 5; // assignment
+    int y = 6;
+    int sum = x + 6;
+
+    std::cout << x << '\n';
+    std::cout << y << '\n';
+    std::cout << sum << '\n';
+    */
+    return 0;
+}
