cc21004fa1acb028d13d97bcd3a67f2ecbca9c40
Lilyana Klecharova <lklecharova9@gmail.com>
2024-10-27 22:05:42 +0200

    const in C++

---
+++ usignConst.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    // The const keyword specifies that a variable's value is constants
+    // tells the compiler to prevent anything from modifying it
+    // (read-only).
+
+    const double PI = 3.14159;
+    const double LIGHT_SPEED = 2999499393;
+    const int WIDTH = 1920;
+    const int HEIGHT = 1080;
+    return 0;
+}
