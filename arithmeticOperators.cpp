dca09c33597d67229fbf5662d474466e290a52c6
Lilyana Klecharova <lklecharova9@gmail.com>
2024-10-27 22:36:55 +0200

    add

---
+++ arithmeticOperators.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+    // arithmetic operators = return the result of a specific
+    // arithmetic operation (+ - * /)
+
+    // parenthesis
+    // multiplication and division
+    // adding and subtraction
+
+    int students = 6 - 5 + 4 * 3 / 2;
+    // students++;
+    // students--;
+    // students *= 2;
+    // student /= 2;
+    // int remainder = students % 2;
+    std::cout << students;
+
+    return 0;
+}
