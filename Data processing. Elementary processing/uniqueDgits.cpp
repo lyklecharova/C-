76dca74fa54771da807d5eb196f1a9ca50a1769c
Lilyana Klecharova <lklecharova9@gmail.com>
2024-10-30 16:36:11 +0200

    new task

---
+++ Data processing. Elementary processing/uniqueDgits.cpp
@@ -0,0 +1,43 @@
+#include <cstdlib>
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    int a;
+    cout << "Enter a four-digit number: ";
+    cin >> a;
+
+    // Extracting the digits of the number
+    int d1 = a / 1000;
+    int d2 = (a / 100) % 10;
+    int d3 = (a / 10) % 10;
+    int d4 = a % 10;
+
+    // // Checking for uniqueness among all digits
+    // if (d1 != d2 && d1 != d3 && d1 != d4 &&
+    //     d2 != d3 && d2 != d4 && d3 != d4)
+    // {
+    //     cout << "1" << endl; // All digits are different
+    // }
+    // else
+    // {
+    //     cout << "0" << endl; // There are duplicate digits
+    // }
+
+    // return 0;
+
+    cout << ((d1 != d2 && d1 != d3 && d1 != d4 &&
+              d2 != d3 && d2 != d4 && d3 != d4)
+                 ? 1
+                 : 0)
+         << endl;
+
+    system("pause");
+    return EXIT_SUCCESS;
+}
