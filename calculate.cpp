df601500410d393cd807dd53aacd6a9f8923bdfb
lyklecharova <lklecharova9@gmail.com>
2024-12-26 16:31:19 +0200

    simple calculator c++

---
+++ calculate.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	double num1, num2;
+	cout << "Enter first number: ";
+	cin >> num1;
+
+	cout << "Enter second number: ";
+	cin >> num2;
+
+	cout << num1 + num2 << endl;
+
+	return 0;
+}
