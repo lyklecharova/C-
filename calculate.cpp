d161e3a40cf29a1f0b14f18f2b1306017c6b6290
lyklecharova <lklecharova9@gmail.com>
2024-12-30 09:18:20 +0200

    add

--- calculate.cpp
+++ calculate.cpp
@@ -1,17 +1,50 @@
 #include <iostream>
-#include <cstdlib>
 using namespace std;
 
 int main()
 {
-	double num1, num2;
-	cout << "Enter first number: ";
-	cin >> num1;
+    int num1, num2;
+    char op;
 
-	cout << "Enter second number: ";
-	cin >> num2;
+    cout << "Enter first number: ";
+    cin >> num1;
 
-	cout << num1 + num2 << endl;
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
 
-	return 0;
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    int result;
+
+    if (op == '+')
+    {
+        result = num1 + num2;
+    }
+    else if (op == '-')
+    {
+        result = num1 - num2;
+    }
+    else if (op == '/')
+    {
+        if (num2 == 0)
+        {
+            cout << "Error: Division by zero is not allowed." << endl;
+            return 1;
+        }
+        result = num1 / num2;
+    }
+    else if (op == '*')
+    {
+        result = num1 * num2;
+    }
+    else
+    {
+        cout << "Invalid operator" << endl;
+        return 1;
+    }
+
+    cout << "Result: " << result << endl;
+
+    return 0;
 }
