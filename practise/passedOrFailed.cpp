2019905b16d6792d0fecb0b39c5bf6bba217f5d9
lyklecharova <lklecharova9@gmail.com>
2025-01-02 10:32:09 +0200

    add

---
+++ practise/passedOrFailed.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+	float grade;
+	cin >> grade;
+
+	if (grade >= 3.00)
+	{
+		cout << "Passed!";
+	}
+	else
+	{
+		cout << "Failed!";
+	}
+
+	return 0;
+}
